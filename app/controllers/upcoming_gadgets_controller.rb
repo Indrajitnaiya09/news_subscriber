7b31f4274d9884e1edda9e73c52d2f1f0956c8eb
Indrajit Naiya <indrajitnaiya09@gmail.com>
2023-09-09 11:28:12 +0530

    Authorization added

--- app/controllers/upcoming_gadgets_controller.rb
+++ app/controllers/upcoming_gadgets_controller.rb
@@ -1,9 +1,10 @@
+require 'pry'
 class UpcomingGadgetsController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:create, :update]
   def create
     @upcoming_gadget = UpcomingGadget.new(upcoming_gadget_params)
-
     if @upcoming_gadget.save
+      render json: {status: 200, passcode: (params["passcode"] + "_sub")}.to_json
     else
     end
   end
@@ -19,6 +20,6 @@ class UpcomingGadgetsController < ApplicationController
   private
 
   def upcoming_gadget_params
-    params.require(:upcoming_gadget).permit(information: {})
+    params.require(:upcoming_gadget).permit(information: {}, passcode: "")
   end
 end
